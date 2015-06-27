a6d25d354d3071c90d9fbf26c3b82bda77f2f606
Alan Choi <alanchoi08@gmail.com>
2015-06-26 23:54:39 -0700

    first commit

---
+++ letter_count.rb
@@ -0,0 +1,16 @@
+#return the word that has the most repeated letters in the string
+#if string has no words with repeats return -1
+#if multiple words have the same number of repeats return the first one
+def repeats(word)
+	
+	i = 0
+	while i < word.length
+
+	end
+end
+
+
+def letter_count(string)
+
+
+end
